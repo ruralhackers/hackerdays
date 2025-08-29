4cb8f14e086a3a22ced5132e85dd61ac569a4a1a
ajamardo <agustin.jch@gmail.com>
2025-08-29 10:55:35 +0200

    Add Netlify adapter and localization support in Astro configuration; update .gitignore to include Netlify folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    locales: ["es", "gl", "en"],
+    defaultLocale: "es",
+  },
+  output: 'server',
+  adapter: node({
+    includeFiles: ['public/locales/**/*'],
+  }),
 });
